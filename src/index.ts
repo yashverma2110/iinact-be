35de43b5caa4d1c5077eb48399e5856767cc2210
yash2110 <yash@quizizz.com>
2022-03-26 03:32:05 +0530

    vercel cors setup

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,14 @@ import listRouter from "./routers/list.router";
 const app: Application = express();
 const port = 8080;
 
+const allowedOrigins = ["http://localhost:3000", "https://iinact.vercel.app"];
+
 // middlwares
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 
 //appending routers to app
